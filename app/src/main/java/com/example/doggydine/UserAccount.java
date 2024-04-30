167a2c0f932735f9ce8a727537efe824752ed19b
이동욱 <du123kim@naver.com>
2024-04-30 14:22:56 +0900

    만드는중

--- app/src/main/java/com/example/doggydine/UserAccount.java
+++ app/src/main/java/com/example/doggydine/UserAccount.java
@@ -7,13 +7,17 @@ public class UserAccount {
     private String idToken;
     private String emailId;
     private String password;
+    private String dog_info;
     private String dog_name;
     private String dog_age;
     private String dog_weight;
     private String active_rate;
     private String allergy;
     private String profile;
-
+    private String profile2;
+    private String profile3;
+    private String profile4;
+    private String profile5;
     public UserAccount() {}
 
     public String getIdToken() { return idToken;}
@@ -53,4 +57,36 @@ public class UserAccount {
     public String getProfile() {return profile;}
 
     public void setProfile(String profile) {this.profile = profile;}
+
+    public String getProfile2() {
+        return profile2;
+    }
+
+    public void setProfile2(String profile2) {
+        this.profile2 = profile2;
+    }
+
+    public String getProfile3() {
+        return profile3;
+    }
+
+    public void setProfile3(String profile3) {
+        this.profile3 = profile3;
+    }
+
+    public String getProfile4() {
+        return profile4;
+    }
+
+    public void setProfile4(String profile4) {
+        this.profile4 = profile4;
+    }
+
+    public String getProfile5() {
+        return profile5;
+    }
+
+    public void setProfile5(String profile5) {
+        this.profile5 = profile5;
+    }
 }
