e989812fc1e6fcccea1b95e3750ef9235899e0cb
이동욱 <du123kim@naver.com>
2024-05-04 19:55:19 +0900

    회원가입 화면 정리 + 중복체크 기능 추가

--- app/src/main/java/com/example/doggydine/Sign_up.java
+++ app/src/main/java/com/example/doggydine/Sign_up.java
@@ -1,6 +1,7 @@
 package com.example.doggydine;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -15,13 +16,17 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -35,6 +40,7 @@ public class Sign_up extends AppCompatActivity {
     private ImageView mImageview;
     private Button mBtnRegister;
     private TextView mTextview;
+    private TextView alertText;
 
     private static final int PICK_IMAGE_REQUEST = 1;
     private Uri selectedImageUri;
@@ -46,6 +52,7 @@ public class Sign_up extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+
         mFirebaseAuth = FirebaseAuth.getInstance();
         mDatabaseRef = FirebaseDatabase.getInstance().getReference("DoggyDine");
 
@@ -55,6 +62,13 @@ public class Sign_up extends AppCompatActivity {
         mBtnRegister = findViewById(R.id.Btn_s_register);
         mImageview = findViewById(R.id.imageView);
         mTextview = findViewById(R.id.Tv_s_go);
+        alertText = findViewById(R.id.tv_alert_email);
+
+        LottieAnimationView lottieAnimationView = findViewById(R.id.Lt_grass);
+        lottieAnimationView.setAnimation(R.raw.grass); // .json 파일을 로드
+        lottieAnimationView.loop(true);
+        lottieAnimationView.playAnimation();
+
 
 
         mTextview.setOnClickListener(new View.OnClickListener() {
@@ -72,11 +86,18 @@ public class Sign_up extends AppCompatActivity {
             }
         });
 
+        mEtEmail.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if(!hasFocus) {
+                    checkEmailDuplicate();
+                }
+            }
+        });
+
         mBtnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
                 // 입력한 텍스트들 가져오기
                 String strEmail = mEtEmail.getText().toString();
                 String strPwd = mEtPwd.getText().toString();
@@ -128,6 +149,38 @@ public class Sign_up extends AppCompatActivity {
             }
         });
 
+    }
+    private void checkEmailDuplicate() {
+        String email = mEtEmail.getText().toString().trim().toLowerCase(); // 이메일을 모두 소문자로 변환하여 비교합니다.
+        mDatabaseRef.child("UserAccount").orderByChild("emailId").equalTo(email).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    // 이미 사용 중인 이메일인 경우
+                    updateUiEmailExists();
+                } else {
+                    // 사용 가능한 이메일인 경우
+                    updateUiEmailAvailable();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // 쿼리 취소 시 처리할 내용
+                Toast.makeText(Sign_up.this, "이메일 중복 체크에 실패했습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void updateUiEmailExists() {
+        alertText.setVisibility(View.VISIBLE);
+        alertText.setText("이미 사용 중인 이메일입니다.");
+    }
+
+    private void updateUiEmailAvailable() {
+        alertText.setVisibility(View.VISIBLE);
+        alertText.setText("사용 가능한 이메일입니다.");
+        alertText.setTextColor(Color.parseColor("#008000"));
     }
 
 
