3d1208438abb4bedd6928f3a538709049167bb53
이동욱 <du123kim@naver.com>
2024-05-26 20:51:54 +0900

    바코드 만드는중

--- app/src/main/java/com/example/doggydine/Feeding.java
+++ app/src/main/java/com/example/doggydine/Feeding.java
@@ -1,7 +1,14 @@
 package com.example.doggydine;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResultLauncher;
@@ -11,17 +18,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import android.content.Intent;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.Spinner;
-import android.widget.TextView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import com.airbnb.lottie.LottieAnimationView;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -38,7 +37,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Feeding extends AppCompatActivity {
-    private String barcode_num;
+    private String barcode_num, saved_barcode_num;
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
@@ -47,11 +46,10 @@ public class Feeding extends AppCompatActivity {
     private FirebaseDatabase database;
     private Spinner mspinner;
     private DatabaseReference databaseReference;
-    private LottieAnimationView mLt_empty;
-    private TextView mTv_emty;
 
     private ImageView mImageview;
     Button btn_scan;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,34 +61,28 @@ public class Feeding extends AppCompatActivity {
             return insets;
         });
 
-
-        recyclerView =findViewById(R.id.d_f_recyclerView);
+        barcode_num = null;
+        saved_barcode_num = null;
+        recyclerView = findViewById(R.id.d_f_recyclerView);
         mImageview = findViewById(R.id.Iv_go_shopping);
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         arrayList = new ArrayList<>();
         originList = new ArrayList<>();
-        mLt_empty = findViewById(R.id.Lt_empty);
-        mLt_empty.setVisibility(View.INVISIBLE);
-        mTv_emty = findViewById(R.id.tv_empty);
-        mTv_emty.setVisibility(View.INVISIBLE);
         database = FirebaseDatabase.getInstance();
         databaseReference = database.getReference("DoggyDine").child("Food");
         String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
         DatabaseReference userCheckRef = FirebaseDatabase.getInstance().getReference("DoggyDine")
                 .child("UserAccount").child(userID).child("check");
 
-
         databaseReference.addValueEventListener(new ValueEventListener() {
-
             @Override
             public void onDataChange(@NonNull DataSnapshot datasnapshot) {
                 arrayList.clear();
                 originList.clear();
 
-
-                for (DataSnapshot snapshot : datasnapshot.getChildren()){
+                for (DataSnapshot snapshot : datasnapshot.getChildren()) {
                     Food food = new Food();
                     food.setProfile(snapshot.child("profile").getValue(String.class));
                     food.setName(snapshot.child("name").getValue(String.class));
@@ -132,22 +124,17 @@ public class Feeding extends AppCompatActivity {
 
                     arrayList.add(food);
                     originList.add(food);
-
-
                 }
                 adapter.notifyDataSetChanged();
-
-
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
+                // 처리할 내용
             }
-
         });
-        adapter = new CustomAdapter(arrayList,this);
+        adapter = new CustomAdapter(arrayList, this);
         recyclerView.setAdapter(adapter);
-
 
         mImageview.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -158,17 +145,15 @@ public class Feeding extends AppCompatActivity {
             }
         });
 
-
-        Button recommend_btn = (Button) findViewById(R.id.recommend_btn);
-        recommend_btn.setOnClickListener(new View.OnClickListener(){
+        Button recommend_btn = findViewById(R.id.recommend_btn);
+        recommend_btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 Intent intent = new Intent(Feeding.this, Recommend.class);
                 startActivity(intent);
                 overridePendingTransition(R.anim.slide_in, R.anim.slide_stay); // 애니메이션 설정
             }
         });
-
 
         // Spinner에서 선택된 정렬 방법을 감지하는 리스너 구현
         mspinner = findViewById(R.id.spinner);
@@ -179,9 +164,7 @@ public class Feeding extends AppCompatActivity {
                     arrayList.clear();
                     arrayList.addAll(originList);
                     adapter.notifyDataSetChanged();
-                }
-
-                else if (position == 1) { // 가격 순 선택
+                } else if (position == 1) { // 가격 순 선택
                     Collections.sort(arrayList, new Comparator<Food>() {
                         @Override
                         public int compare(Food food1, Food food2) {
@@ -232,7 +215,6 @@ public class Feeding extends AppCompatActivity {
                 }
 
                 adapter.notifyDataSetChanged();
-
             }
 
             @Override
@@ -242,10 +224,11 @@ public class Feeding extends AppCompatActivity {
         });
 
         btn_scan = findViewById(R.id.barcode_btn);
-        btn_scan.setOnClickListener(v->{
+        btn_scan.setOnClickListener(v -> {
             scanCode();
         });
     }
+
     @Override
     public void onBackPressed() {
         super.onBackPressed();
@@ -254,9 +237,7 @@ public class Feeding extends AppCompatActivity {
         finish();
     }
 
-
-
-    private void scanCode(){
+    private void scanCode() {
         ScanOptions options = new ScanOptions();
         options.setPrompt("Volume up to flash on");
         options.setBeepEnabled(true);
@@ -264,29 +245,44 @@ public class Feeding extends AppCompatActivity {
         options.setCaptureActivity(CaptureAct.class);
         barLauncher.launch(options);
     }
-    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result->{
-        if(result.getContents() != null){
+
+    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result -> {
+        if (result.getContents() != null) {
             // 바코드 결과를 변수에 저장
             barcode_num = result.getContents();
-            //Test용
+            // Test용
             Log.d("Barcode", "Scanned Barcode Number: " + barcode_num);
 
-            Intent intent = new Intent();
-            intent.putExtra("FromBarcode",true);
-            intent.putExtra("Barcode_num",barcode_num);
-
-
+            // AlertDialog 생성 및 표시
             AlertDialog.Builder builder = new AlertDialog.Builder(Feeding.this);
-            builder.setTitle("Result");
-            builder.setMessage(result.getContents());
-            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            builder.setTitle("알림");
+            builder.setMessage("등록하시겠습니까?");
+            builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
                     dialogInterface.dismiss();
-                    //일단여기에 test용으로 달아두자
-                    startActivity(intent);
+                    saved_barcode_num = barcode_num;
+                    processBarcode(saved_barcode_num);
+                    saved_barcode_num=null;
+                    barcode_num=null;
+                    //Toast.makeText(Feeding.this, "상품이 등록되었습니다.", Toast.LENGTH_SHORT).show();
+                }
+            });
+            builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                    barcode_num = null;
                 }
             }).show();
         }
     });
+
+    private void processBarcode(String barcode) {
+        // 바코드 번호를 사용하여 필요한 작업 수행
+        if (barcode != null && !barcode.isEmpty()) {
+            Log.d("Barcode", "Processed Barcode Number: " + barcode);
+        }
+    }
+
 }
