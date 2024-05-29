d08508cbb4f2e2a5d879fa2cde2c0f8a14132c86
yunJ00 <heather2000@naver.com>
2024-05-30 00:38:39 +0900

    Fix: DogSignUp도 하루 급여횟수 잘 적용되게 수정

--- app/src/main/java/com/example/doggydine/DogSignUp.java
+++ app/src/main/java/com/example/doggydine/DogSignUp.java
@@ -365,9 +365,11 @@ public class DogSignUp extends AppCompatActivity {
             String activationRate = String.valueOf(averageValue);
             activeTextView.setText(activationRate);
         } else if (requestCode == 2 && resultCode == RESULT_OK && data != null) {
-            int numberValue = data.getIntExtra("number Value", 0);
-            String numberRate = String.valueOf(numberValue);
-            how_much_text.setText(numberRate);
+            int numberValue = data.getIntExtra("numberValue", -1);
+            if (numberValue != -1) {
+                String numberRate = String.valueOf(numberValue);
+                how_much_text.setText(numberRate);
+            }
         }
     }
 
