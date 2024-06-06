912d3b815b7f12ef91f0ff35dad44ddd2dd46121
yunJ00 <heather2000@naver.com>
2024-06-07 01:14:45 +0900

    Feat: 로그아웃 기능 추가

--- app/src/main/java/com/example/doggydine/Setting.java
+++ app/src/main/java/com/example/doggydine/Setting.java
@@ -1,7 +1,14 @@
 package com.example.doggydine;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -30,6 +37,7 @@ public class Setting extends AppCompatActivity {
     private ArrayList<PetAccount> arrayList;
     private DatabaseReference databaseReference;
     private FirebaseAuth mFirebaseAuth;
+    Button logOut, connecting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +53,9 @@ public class Setting extends AppCompatActivity {
         lottieAnimationView.setAnimation(R.raw.dog_sleep); // .json 파일을 로드
         lottieAnimationView.loop(true);
         lottieAnimationView.playAnimation();
+
+        logOut = findViewById(R.id.logout_btn);
+        connecting = findViewById(R.id.bluetooth_btn);
 
         recyclerView = findViewById(R.id.DogInfoEdit);
         recyclerView.setHasFixedSize(true);
@@ -74,6 +85,39 @@ public class Setting extends AppCompatActivity {
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
+            }
+        });
+
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(Setting.this);
+                builder.setTitle("로그아웃 확인");
+                builder.setMessage("정말 로그아웃하시겠습니까?");
+                builder.setPositiveButton("로그아웃", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        FirebaseAuth.getInstance().signOut();
+
+                        SharedPreferences sharedPreferences = getSharedPreferences("login_info", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.remove("username");
+                        editor.remove("password");
+                        editor.apply();
+
+                        Intent intent = new Intent(Setting.this, Login.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+                builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
             }
         });
 
